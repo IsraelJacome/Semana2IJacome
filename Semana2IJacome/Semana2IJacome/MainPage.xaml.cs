da0317f3d9fa9686e73422da7c7873d39d4eab0d
ERMES_DMS <ERMES_DMS@PC>
2020-11-06 15:21:31 -0500

    Solucion

---
+++ Semana2IJacome/Semana2IJacome/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana2IJacome
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private async void btnAbrir_ventana_dos_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            string user_val = "estudiante2020";
+            string pass_val = "uisrael2020";
+
+            if (pass == pass_val)
+            {
+                await Navigation.PushAsync(new Ventana2(user, pass));
+            }
+          
+   
+
+          
+        }
+    }
+}
---
+++ Semana2IJacome/Semana2IJacome/Ventana2.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana2IJacome
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        public Ventana2(string user, string pass)
+        {
+            InitializeComponent();
+            lblUser.Text = user;
+            txtPass.Text = pass;
+
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+            // nota seguimiento 1
+            double parcial1 = Convert.ToDouble(txtParcial1.Text);
+            double examen1 = Convert.ToDouble(txtExamen1.Text);
+            double nota1 = (parcial1 * 0.3) + (examen1 * 0.2);
+
+            //nota seguimiento 2
+            double parcial2 = Convert.ToDouble(txtParcial2.Text);
+            double examen2 = Convert.ToDouble(txtExamen2.Text);
+            double nota2 = (parcial2 * 0.3) + (examen2 * 0.2);
+
+            // calculo nota final
+            double notaFinal = nota1 + nota2;
+
+            //envio de resultados
+            txtResultado.Text = notaFinal.ToString();
+
+            if (notaFinal >= 7)
+            {
+                txtEstado.Text = "Aprobado";
+
+            }
+            else
+            {
+                txtEstado.Text = "Reprobado";
+            }
+        }
+    }
+}
